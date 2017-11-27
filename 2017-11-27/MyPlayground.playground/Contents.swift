28984b05b9da3a514ff23e67f728cff06215b5b7
Jason Liang <liangjyjason@gmail.com>
2017-11-27 11:07:19 -0500

    playground

---
+++ 2017-11-27/MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str:String? = "Hello, playground"
+str = nil
+
+var a:String = "hello"
+var b:String = "World"
+var res = a + " " + b
+var res2 = "\(a) \(b)"
+
+var value:Float = 1
+
+res == res2
+
+var array : [Any] = ["1",1]
+array.append("stuff")
+array.append("stuff2")
+array.append(contentsOf: ["1","2"])
+
+var twoDArray :[[String]] = []
+
+twoDArray.append(["1","2"])
+
+
+
+
+
+
+
