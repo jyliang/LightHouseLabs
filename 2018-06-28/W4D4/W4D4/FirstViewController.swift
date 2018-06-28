e51405be1e6aefdb7048a59879d9d6e88a1d13f5
Jason Liang <liangjyjason@gmail.com>
2018-06-28 10:01:15 -0400

    Initialize Project W4D4

---
+++ 2018-06-28/W4D4/W4D4/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  W4D4
+//
+//  Created by Jason Liang on 6/28/18.
+//  Copyright © 2018 jasonliang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
